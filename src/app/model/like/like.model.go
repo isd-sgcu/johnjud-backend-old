92623a8c932531097c06da1dd9c675bb18362640
Idhibhat Pankam <marginpankam@gmail.com>
2023-12-21 17:10:04 +0700

    feat: like model

---
+++ src/app/model/like/like.model.go
@@ -0,0 +1,17 @@
+package like
+
+import (
+	"os/user"
+
+	"github.com/google/uuid"
+	"github.com/isd-sgcu/johnjud-backend/src/app/model"
+	"github.com/isd-sgcu/johnjud-backend/src/app/model/pet"
+)
+
+type Like struct {
+	model.Base
+	PetID  *uuid.UUID `json:"pet_id" gorm:"index:idx_name,unique"`
+	Pet    *pet.Pet   `json:"pet" gorm:"foreignKey:PetID;constraint:OnUpdate:CASCADE;OnDelete:SET NULL;"`
+	UserID *uuid.UUID `json:"user_id" gorm:"index:idx_name,unique"`
+	User   *user.User `json:"user" gorm:"foreignKey:UserID;constraint:OnUpdate:CASCADE;OnDelete:SET NULL;"`
+}
