cb62e7e660810b699b5b6fae1d253979db877c41
Idhibhat Pankam <marginpankam@gmail.com>
2023-12-27 21:51:45 +0700

    feat: main init db, conf, net

--- src/main.go
+++ src/main.go
@@ -1,5 +1,36 @@
 package main
 
+import (
+	"fmt"
+	"net"
+
+	"github.com/isd-sgcu/johnjud-backend/src/config"
+	"github.com/isd-sgcu/johnjud-backend/src/database"
+	"github.com/rs/zerolog/log"
+)
+
 func main() {
-	// Code
+	conf, err := config.LoadConfig()
+	if err != nil {
+		log.Fatal().
+			Err(err).
+			Str("service", "backend load config").
+			Msg("Failed to start service")
+	}
+
+	db, err := database.InitPostgresDatabase(&conf.Database, conf.App.Debug)
+	if err != nil {
+		log.Fatal().
+			Err(err).
+			Str("service", "backend database").
+			Msg("Failed to start service")
+	}
+
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%v", conf.App.Port))
+	if err != nil {
+		log.Fatal().
+			Err(err).
+			Str("service", "backend server").
+			Msg("Failed to start service")
+	}
 }
