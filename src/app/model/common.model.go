92990f907b9bf390b6dc5236de906cfad9911161
Idhibhat Pankam <marginpankam@gmail.com>
2023-12-27 22:39:41 +0700

    fix: gorm postgres model

--- src/app/model/common.model.go
+++ src/app/model/common.model.go
@@ -8,10 +8,10 @@ import (
 )
 
 type Base struct {
-	ID        uuid.UUID      `json:"id" gorm:"primaryKey;size:191"`
-	CreatedAt time.Time      `json:"created_at" gorm:"type:datetime;autoCreateTime:nano"`
-	UpdatedAt time.Time      `json:"updated_at" gorm:"type:datetime;autoUpdateTime:nano"`
-	DeletedAt gorm.DeletedAt `json:"deleted_at" gorm:"index;type:datetime"`
+	ID        uuid.UUID      `json:"id" gorm:"primary_key"`
+	CreatedAt time.Time      `json:"created_at" gorm:"type:timestamp;autoCreateTime:nano"`
+	UpdatedAt time.Time      `json:"updated_at" gorm:"type:timestamp;autoUpdateTime:nano"`
+	DeletedAt gorm.DeletedAt `json:"deleted_at" gorm:"index;type:timestamp"`
 }
 
 func (m *Base) BeforeCreate(_ *gorm.DB) error {
